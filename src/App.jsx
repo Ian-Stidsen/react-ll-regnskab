a9cae3d8840acab1cc99f87f77865866d63a115c
Ian Stidsen <ianlundstidsen@gmail.com>
2023-01-14 11:11:21 -0300

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { lazy } from'react';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
 } from 'react-router-dom';
 
 import Footer from './components/Footer';
@@ -24,31 +24,36 @@ const GrlHome = lazy(() => import('./pages/grl/Home'));
 const GrlAbout = lazy(() => import('./pages/grl/About'));
 const GrlContact = lazy(() => import('./pages/grl/Contact'));
 
+
+
 function App() {
+
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Navbar />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Route>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Navbar />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Route>
 
-        <Route path='/en/' element={<EnNavbar />}>
-          <Route path="/en/" element={<EnHome />} />
-          <Route path="/en/about" element={<EnAbout />} />
-          <Route path="/en/contact" element={<EnContact />} />
-        </Route>
+          <Route path='/en/' element={<EnNavbar />}>
+            <Route path="/en/" element={<EnHome />} />
+            <Route path="/en/about" element={<EnAbout />} />
+            <Route path="/en/contact" element={<EnContact />} />
+          </Route>
 
-        <Route path='/grl/' element={<GrlNavbar />}>
-          <Route path="/grl/" element={<GrlHome />} />
-          <Route path="/grl/about" element={<GrlAbout />} />
-          <Route path="/grl/contact" element={<GrlContact />} />
-        </Route>
+          <Route path='/grl/' element={<GrlNavbar />}>
+            <Route path="/grl/" element={<GrlHome />} />
+            <Route path="/grl/about" element={<GrlAbout />} />
+            <Route path="/grl/contact" element={<GrlContact />} />
+          </Route>
 
-      </Routes>
-      <Footer />
-    </Router>
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
